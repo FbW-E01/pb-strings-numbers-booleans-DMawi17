31b77f363ba997cef283d4936f9a6c8f34d16294
Dagmawi <asegda17@gmail.com>
2021-03-30 17:07:31 +0200

    Strings, Numbers and Booleans

---
+++ main.js
@@ -0,0 +1,23 @@
+const veg = 'Dagmawi';
+console.log(veg);
+
+console.log(veg[1], veg[5]);
+
+const name = 'John';
+const age = 30;
+const year = 2021;
+
+console.log(`${name} is ${age} in ${year}`)
+
+
+const isMarried = true;
+console.log(`${name} is married: ${1>0}`)
+
+const programmingLanguage = 'JavaScript';
+const isFun = true;
+
+console.log(`${programmingLanguage} is fun: ${isFun}`)
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+console.log(programmingLanguage.length);
